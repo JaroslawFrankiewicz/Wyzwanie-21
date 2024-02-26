6a76632a65f4c2dd38ecc62ed39a1281a02c7210
Jarek Frankiewicz <frantz12@wp.pl>
2024-02-26 18:38:50 +0100

    Add project files.

---
+++ Wyzwanie 21 - 2.Test/UserTest.cs
@@ -0,0 +1,52 @@
+using System.Reflection.Metadata;
+
+namespace Wyzwanie_21.Test
+{
+    public class Tests
+    {
+
+        [Test]
+        public void sumOfTwoScoresTest()
+        {
+            // arrange
+            var user = new User("Adam", "1234");
+            user.AddScore(5);
+            user.AddScore(6);
+
+            // act
+            var result = user.Result;
+            // assert
+            Assert.AreEqual(11, result);
+        }
+
+        [Test]
+        public void sumOfThreeScoresTest()
+        {
+            // arrange
+            var user = new User("Adam", "1234");
+            user.AddScore(5);
+            user.AddScore(6);
+            user.AddScore(7);
+            
+            // act
+            var result = user.Result;
+            // assert
+            Assert.AreEqual(18, result);
+        }
+        [Test]
+        public void sumOfFourScoresTest()
+        {
+            // arrange
+            var user = new User("Adam", "1234");
+            user.AddScore(5);
+            user.AddScore(6);
+            user.AddScore(7);
+            user.AddScore(-18);
+
+            // act
+            var result = user.Result;
+            // assert
+            Assert.AreEqual(0, result);
+        }
+    } 
+} 
---
+++ Wyzwanie 21 - 2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Wyzwanie_21;
+
+User user1 = new User("Adam", "1234");
+User user2 = new User("Monika", "5678");
+User user3 = new User("Zuzia", "9012");
+User user4 = new User("Damian", "3456");
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine("Użytkownik " + user1.Login + " z liczbą punktów: " +  result);
