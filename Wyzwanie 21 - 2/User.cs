2a08c2fd95f4b1b0d9dfcb227e543851d0e3e2f4
Jarek Frankiewicz <frantz12@wp.pl>
2024-02-27 20:30:10 +0100

    Zadanie Day8

--- Wyzwanie 21 - 2/User.cs
+++ Wyzwanie 21 - 2/User.cs
@@ -10,6 +10,11 @@ namespace Wyzwanie_21
     {
         private List<int> score = new List<int>();
 
+        public User(string login)
+        {
+            this.Login = login;
+        }
+
         public User(string login, string password)
         {
             this.Login = login;
