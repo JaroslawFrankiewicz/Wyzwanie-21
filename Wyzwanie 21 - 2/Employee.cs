3ddafcb231ceb95a6b792a1e88ceff9301919201
Jarek Frankiewicz <frantz12@wp.pl>
2024-03-05 17:32:48 +0100

    Day11

--- Wyzwanie 21 - 2/Employee.cs
+++ Wyzwanie 21 - 2/Employee.cs
@@ -1,5 +1,4 @@
 ﻿
-
 namespace Wyzwanie_21
 {
     public class Employee
@@ -70,7 +69,47 @@ namespace Wyzwanie_21
             }
         }
 
-        public Statistics GetStatistics()
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Sum = 0;
+            var index = 0;
+
+            while (index < this.grades.Count)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                statistics.Sum += this.grades[index];
+                index++;
+            }
+            statistics.Average /= this.grades.Count;
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Sum = 0;
+            var index = 0;
+            do
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                statistics.Sum += this.grades[index];
+                index++;
+            } while (index < this.grades.Count);
+            statistics.Average /= this.grades.Count;
+            return statistics;
+        }
+        public Statistics GetStatisticsWithForEach()
         {
             var statistics = new Statistics();
             statistics.Average = 0;
@@ -85,7 +124,25 @@ namespace Wyzwanie_21
                 statistics.Average += grade;
                 statistics.Sum += grade;
             }
+            statistics.Average /= this.grades.Count;
+            return statistics;
+        }
 
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Sum = 0;
+
+            for (var index = 0; index < this.grades.Count; index++)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                statistics.Sum += this.grades[index];
+            }
             statistics.Average /= this.grades.Count;
             return statistics;
         }
